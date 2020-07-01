da00098eec1cc11e5f220ba13d6b71917828d06e
BecaRen <65912834+BecaRen@users.noreply.github.com>
2020-07-01 23:37:24 +0200

    Update script.js
    This is the update after I managed to add the second alert&reset option

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+let humanGuess = 0;
+let computerGuess = 0;
+
 // Generate random number between 0 and 9 that will be the target number
 const generateTarget = () => Math.floor(Math.random() * 10);
-
-// Alert human user that they chose a number out of range
-const alert = humanGuess => {
-    if (humanGuess < 0 || humanGuess > 9) {
-        console.log('Please type a number between 0 and 9.');
-    } 
-}
+let randomNumber = generateTarget();
 
 // Calculate absolute distance between guess and target numbers
 const getAbsoluteDistance = (x, y) => Math.abs(x-y);
+
+function restart() {
+    window.alert('Must pick number between 0 and 9');
+    // Reset the guess input box and the target number display:
+  targetNumberDisplay.innerText = '?';
+  guessButton.innerText = 'Make a Guess';
+  humanGuessInput.value = '';
+  computerGuessDisplay.innerText = '?';
+  computerWinsDisplay.innerText = '';
+  guessButton.classList.remove('winning-text');
+}
     
 // Guesses comparison. If human guess is closer to target number than or equal to computer guess, human wins.
 const compareGuesses = (humanGuess, computerGuess, randomNumber) => {
+ 
+    if (humanGuess < 0 || humanGuess > 9) {
+        return restart();
+    }
 
     if (getAbsoluteDistance(humanGuess, randomNumber) <= getAbsoluteDistance(computerGuess, randomNumber)) {    
-        return true;        
-    } else {
-        return false;        
-    }
+            return true;        
+        } else {
+            return false;        
+        }
+    
+
 }
 
 
